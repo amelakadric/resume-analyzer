3721a40f803817b37cd73816d014b3ef694c3754
amelakadric <amela00108@gmail.com>
2025-05-11 12:53:47 -0400

    Create server wake-up request #23

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 import ResumeAnalyzerPage from "./pages/ResumeAnalyzerPage";
+import { getHealth } from "./services/healthService";
 
 function App() {
+  useEffect(() => {
+    getHealth();
+  }, []);
+
   return (
     <>
       <ResumeAnalyzerPage />
